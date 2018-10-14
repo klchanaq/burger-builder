3a29568dbf2d3b7d1bcd5c19f410c0c3bf1f0922
klchanaq <chankwokleung21@gmail.com>
2018-10-14 22:22:44 +0800

    1. Add SimpleUserDTO 2. Add Initial AccountResource Controller

--- src/main/java/com/example/burgerbuilder/service/dto/SimpleUserDTO.java
+++ src/main/java/com/example/burgerbuilder/service/dto/SimpleUserDTO.java
@@ -1,4 +1,71 @@
 package com.example.burgerbuilder.service.dto;
 
-public class SimpleUserDTO {
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A DTO representing a user, with his/her authorities.
+ */
+public class SimpleUserDTO implements Serializable {
+
+    private static final long serialVersionUID = 2124L;
+
+    @NotBlank
+    @Email
+    @Size(min = 5, max = 50)
+    private String email;
+
+    @NotBlank
+    @Size(min = 4, max = 100)
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public SimpleUserDTO email(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public SimpleUserDTO password(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SimpleUserDTO that = (SimpleUserDTO) o;
+        return Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleUserDTO{" +
+                "email='" + email + '\'' +
+                '}';
+    }
+
 }
--- src/main/java/com/example/burgerbuilder/web/rest/AccountResource.java
+++ src/main/java/com/example/burgerbuilder/web/rest/AccountResource.java
@@ -1,4 +1,56 @@
 package com.example.burgerbuilder.web.rest;
 
+import com.example.burgerbuilder.domain.Customer;
+import com.example.burgerbuilder.service.CustomerService;
+import com.example.burgerbuilder.service.dto.SimpleUserDTO;
+import com.example.burgerbuilder.web.rest.util.HeaderUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * REST controller for managing User Account.
+ */
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(
+        origins = {"*"},
+        allowedHeaders = {"*"},
+        exposedHeaders = {"X-burgerBuilder-alert", "X-burgerBuilder-params", "X-burgerBuilder-error"},
+        maxAge = 3600)
 public class AccountResource {
+
+    private final Logger log = LoggerFactory.getLogger(AccountResource.class);
+
+    private static final String ENTITY_NAME = "customer";
+
+    private final CustomerService customerService;
+
+    public AccountResource(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    /**
+     * POST  /register : register the user.
+     *
+     * @param simpleUserDTO the managed user View Model
+     * @throws URISyntaxException
+     */
+    @PostMapping("/register")
+    public ResponseEntity<Customer> registerAccount(@Valid @RequestBody SimpleUserDTO simpleUserDTO) throws URISyntaxException {
+        Customer customer = new Customer();
+        customer.email(simpleUserDTO.getEmail())
+                .password(simpleUserDTO.getPassword());
+        Customer result = customerService.save(customer);
+        return ResponseEntity.created(new URI("/api/customers/" + result.getId()))
+                .headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString()))
+                .body(result);
+    }
+
 }
